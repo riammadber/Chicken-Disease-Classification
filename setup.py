3200e822bfaf2923066304955cc55967835c0441
Md  Riam Madber <ahmedriam00268@gmail.com>
2026-05-05 03:59:54 +0000

    folder structure and setup added

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "End-to-End-Deep-Learning-Project-Chicken-Disease"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "riam"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "riammadbor@gmail.com"
 
 
 setuptools.setup(
